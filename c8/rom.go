b87d739824f489510070beb6e34de524426feb80
Erdinc Mutlu <erdinc@luno.com>
2019-08-10 20:26:54 +0100

    Check stack length before trying to return from subroutine & code clean up

--- c8/rom.go
+++ c8/rom.go
@@ -63,28 +63,32 @@ func ReadROM(filename string) error {
 
 // RunROM will run the ROM
 func RunROM() error {
-	for regs.progCounter <= memorySize {
-		fmt.Printf("At instruction 0x%X: ", regs.progCounter)
+	for regs.progCounter < memorySize {
 		b1 := uint16(memory[regs.progCounter])
 		b2 := uint16(memory[regs.progCounter+1])
 		val := b1<<8 + b2
+		fmt.Printf("At instruction 0x%X: 0x%x ", regs.progCounter, val)
 		switch {
 		case val == 0x0000:
 			// 0000 NOP No Operation
-			fmt.Printf("0x%X No operation\n", val)
+			fmt.Printf("No operation\n")
 
 		case val == 0x00E0:
 			// 00E0	Display	disp_clear()	Clears the screen.
 			pixels = [pixelsVertically][pixelsHorizontally]byte{}
-			fmt.Printf("0x%X Clear the screen\n", val)
+			fmt.Printf("Clear the screen\n")
 
 		case val == 0x00EE:
 			// 00EE	Flow	return;	Returns from a subroutine.
-			regs.v = stack[len(stack)-1].v
-			regs.index = stack[len(stack)-1].index
-			regs.progCounter = stack[len(stack)-1].progCounter
-			stack = stack[:len(stack)-1]
-			fmt.Printf("0x%X Return from a subroutine\n", val)
+			if len(stack) == 0 {
+				fmt.Printf("Return from a subroutine => Cannot return as stack is empty\n")
+			} else {
+				regs.v = stack[len(stack)-1].v
+				regs.index = stack[len(stack)-1].index
+				regs.progCounter = stack[len(stack)-1].progCounter
+				stack = stack[:len(stack)-1]
+				fmt.Printf("Return from a subroutine\n")
+			}
 
 		case val <= 0x0FFF:
 			// 0NNN	Call		Calls RCA 1802 program at address NNN. Not necessary for most ROMs.
@@ -92,23 +96,23 @@ func RunROM() error {
 			for i := range regs.v {
 				regs.v[i] = 0
 			}
-			fmt.Printf("0x%X Call RCA 1802 program at address 0x%X\n", val, val&0xFFF)
+			fmt.Printf("Call RCA 1802 program at address 0x%X\n", val&0xFFF)
 
 		case val >= 0x1000 && val <= 0x1FFF:
 			// 1NNN	Flow	goto NNN;	Jumps to address NNN.
 			regs.progCounter = val&0xFFF - 2
-			fmt.Printf("0x%X Jump to address at 0x%X\n", val, val&0xFFF)
+			fmt.Printf("Jump to address at 0x%X\n", val&0xFFF)
 
 		case val >= 0x2000 && val <= 0x2FFF:
 			// 2NNN	Flow	*(0xNNN)()	Calls subroutine at NNN.
 			regs.progCounter = val&0xFFF - 2
 			stack = append(stack, stackPtr{v: regs.v, index: regs.index, progCounter: regs.progCounter})
-			fmt.Printf("0x%X Call subroutine at 0x%X\n", val, val&0xFFF)
+			fmt.Printf("Call subroutine at 0x%X\n", val&0xFFF)
 
 		case val >= 0x3000 && val <= 0x3FFF:
 			// 3XNN	Cond	if(Vx==NN)	Skips the next instruction if VX equals NN.
 			// (Usually the next instruction is a jump to skip a code block)
-			fmt.Printf("0x%X Skip next instr if V%X (val:%d) equals 0x%X", val, b1&0x0F, regs.v[b1&0x0F], b2)
+			fmt.Printf("Skip next instr if V%X (val:%d) equals 0x%X", b1&0x0F, regs.v[b1&0x0F], b2)
 			if regs.v[b1&0x0F] == byte(b2) {
 				regs.progCounter += 2
 				fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -119,7 +123,7 @@ func RunROM() error {
 		case val >= 0x4000 && val <= 0x4FFF:
 			// 4XNN	Cond	if(Vx!=NN)	Skips the next instruction if VX doesn't equal NN.
 			// (Usually the next instruction is a jump to skip a code block)
-			fmt.Printf("0x%X Skip next instr if V%X (val:%d) doesn't equal 0x%X", val, b1&0x0F, regs.v[b1&0x0F], b2)
+			fmt.Printf("Skip next instr if V%X (val:%d) doesn't equal 0x%X", b1&0x0F, regs.v[b1&0x0F], b2)
 			if regs.v[b1&0x0F] != byte(b2) {
 				regs.progCounter += 2
 				fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -132,8 +136,8 @@ func RunROM() error {
 			case 0x0:
 				// 5XY0	Cond	if(Vx==Vy)	Skips the next instruction if VX equals VY.
 				// (Usually the next instruction is a jump to skip a code block)
-				fmt.Printf("0x%X Skip next instr if V%X (val:%d) equals V%X (val:%d)",
-					val, b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4])
+				fmt.Printf("Skip next instr if V%X (val:%d) equals V%X (val:%d)",
+					b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4])
 				if regs.v[b1&0x0F] == regs.v[b2&0xF0>>4] {
 					regs.progCounter += 2
 					fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -148,35 +152,35 @@ func RunROM() error {
 		case val >= 0x6000 && val <= 0x6FFF:
 			// 6XNN	Const	Vx = NN	Sets VX to NN.
 			regs.v[b1&0x0F] = byte(b2)
-			fmt.Printf("0x%X Set V%X to 0x%X (%d)\n", val, b1&0x0F, b2, b2)
+			fmt.Printf("Set V%X to 0x%X (%d)\n", b1&0x0F, b2, b2)
 
 		case val >= 0x7000 && val <= 0x7FFF:
 			// 7XNN	Const	Vx += NN	Adds NN to VX. (Carry flag is not changed)
 			regs.v[b1&0x0F] = regs.v[b1&0x0F] + byte(b2)
-			fmt.Printf("0x%X Add %d to V%X (final val:%d)\n", val, b2, b1&0x0F, regs.v[b1&0x0F])
+			fmt.Printf("Add %d to V%X (final val:%d)\n", b2, b1&0x0F, regs.v[b1&0x0F])
 
 		case val >= 0x8000 && val <= 0x8FFF:
 			switch b2 & 0xF {
 			case 0x0:
 				// 8XY0	Assign	Vx=Vy	Sets VX to the value of VY.
 				regs.v[b1&0x0F] = regs.v[b2&0xF0>>4]
-				fmt.Printf("0x%X Set V%X to value of V%X (val:%d)\n",
-					val, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
+				fmt.Printf("Set V%X to value of V%X (val:%d)\n",
+					b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
 			case 0x1:
 				// 8XY1	BitOp	Vx=Vx|Vy	Sets VX to VX or VY. (Bitwise OR operation)
 				regs.v[b1&0x0F] = regs.v[b1&0x0F] ^ regs.v[b2&0xF0>>4]
-				fmt.Printf("0x%X Set V%X to bitwise V%X or V%X (Final val:%d)\n",
-					val, b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
+				fmt.Printf("Set V%X to bitwise V%X or V%X (Final val:%d)\n",
+					b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
 			case 0x2:
 				// 8XY2	BitOp	Vx=Vx&Vy	Sets VX to VX and VY. (Bitwise AND operation)
 				regs.v[b1&0x0F] = regs.v[b1&0x0F] & regs.v[b2&0xF0>>4]
-				fmt.Printf("0x%X Set V%X to bitwise V%X and V%X (Final val:%d)\n",
-					val, b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
+				fmt.Printf("Set V%X to bitwise V%X and V%X (Final val:%d)\n",
+					b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
 			case 0x3:
 				// 8XY3	BitOp	Vx=Vx^Vy	Sets VX to VX xor VY.
 				regs.v[b1&0x0F] = regs.v[b1&0x0F] ^ regs.v[b2&0xF0>>4]
-				fmt.Printf("0x%X Set V%X to bitwise V%X xor V%X (Final val:%d)\n",
-					val, b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
+				fmt.Printf("Set V%X to bitwise V%X xor V%X (Final val:%d)\n",
+					b1&0x0F, b1&0x0F, b2&0xF0>>4, regs.v[b1&0x0F])
 			case 0x4:
 				// 8XY4	Math	Vx += Vy	Adds VY to VX. VF is set to 1 when
 				// there's a carry, and to 0 when there isn't.
@@ -187,8 +191,8 @@ func RunROM() error {
 				} else {
 					regs.v[0xF] = 0
 				}
-				fmt.Printf("0x%X Set V%X to V%X (val:%d) + V%X (val:%d) (Final val:%d)",
-					val, b1&0x0F, b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4], total)
+				fmt.Printf("Set V%X to V%X (val:%d) + V%X (val:%d) (Final val:%d)",
+					b1&0x0F, b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4], total)
 				if regs.v[0xF] == 1 {
 					fmt.Printf(" => CARRY OVER\n")
 				} else {
@@ -205,8 +209,8 @@ func RunROM() error {
 				} else {
 					regs.v[0xF] = 0
 				}
-				fmt.Printf("0x%X Set V%X to V%X (val:%d) - V%X (val:%d) (Final val:%d)",
-					val, b1&0x0F, b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4], sub)
+				fmt.Printf("Set V%X to V%X (val:%d) - V%X (val:%d) (Final val:%d)",
+					b1&0x0F, b1&0x0F, regs.v[b1&0x0F], b2&0xF0>>4, regs.v[b2&0xF0>>4], sub)
 				if regs.v[0xF] == 1 {
 					fmt.Printf(" => BORROWED\n")
 				} else {
@@ -218,8 +222,8 @@ func RunROM() error {
 				oldVal := regs.v[b1&0x0F]
 				regs.v[b2&0xF0>>4] = regs.v[b1&0x0F] & 1
 				regs.v[b1&0x0F] = regs.v[b1&0x0F] >> 1
-				fmt.Printf("0x%X Store the least significant bit of V%X (val:%d) in V%X then shift V%X to the right by 1 (final val:%d)\n",
-					val, b1&0x0F, oldVal, b2&0xF0>>4, regs.v[b2&0xF0>>4], regs.v[b1&0x0F])
+				fmt.Printf("Store the least significant bit of V%X (val:%d) in V%X then shift V%X to the right by 1 (final val:%d)\n",
+					b1&0x0F, oldVal, b2&0xF0>>4, regs.v[b2&0xF0>>4], regs.v[b1&0x0F])
 			case 0x7:
 				// 8XY7	Math	Vx=Vy-Vx	Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
 				sub := int(regs.v[b2&0xF0>>4]) - int(regs.v[b1&0x0F])
@@ -229,8 +233,8 @@ func RunROM() error {
 				} else {
 					regs.v[0xF] = 0
 				}
-				fmt.Printf("0x%X Set V%X to V%X (val:%d) - V%X (val:%d) (Final val:%d)",
-					val, b1&0x0F, b2&0xF0>>4, regs.v[b2&0xF0>>4], b1&0x0F, regs.v[b1&0x0F], sub)
+				fmt.Printf("Set V%X to V%X (val:%d) - V%X (val:%d) (Final val:%d)",
+					b1&0x0F, b2&0xF0>>4, regs.v[b2&0xF0>>4], b1&0x0F, regs.v[b1&0x0F], sub)
 				if regs.v[0xF] == 1 {
 					fmt.Printf(" => BORROWED\n")
 				} else {
@@ -242,8 +246,8 @@ func RunROM() error {
 				oldVal := regs.v[b1&0x0F]
 				regs.v[b2&0xF0>>4] = regs.v[b1&0x0F] & 0x80
 				regs.v[b1&0x0F] = regs.v[b1&0x0F] << 1
-				fmt.Printf("0x%X Store the most significant bit of V%X (val:%d) in V%X then shift V%X to the left by 1 (final val:%d)\n",
-					val, b1&0x0F, oldVal, b2&0xF0>>4, regs.v[b2&0xF0>>4], regs.v[b1&0x0F])
+				fmt.Printf("Store the most significant bit of V%X (val:%d) in V%X then shift V%X to the left by 1 (final val:%d)\n",
+					b1&0x0F, oldVal, b2&0xF0>>4, regs.v[b2&0xF0>>4], regs.v[b1&0x0F])
 			default:
 				fmt.Printf("-------------> Unknown statement !!! Data:0x%X\n", val)
 				return nil
@@ -252,8 +256,8 @@ func RunROM() error {
 		case val >= 0x9000 && val <= 0x9FFF:
 			// 9XY0	Cond	if(Vx!=Vy)	Skips the next instruction if VX doesn't equal VY.
 			// (Usually the next instruction is a jump to skip a code block)
-			fmt.Printf("0x%X Skip next instr if V%X (val:%d) doesn't equal to V%X (val:%d)",
-				val, b1&0xF, regs.v[b1&0xF], b2&0xF0>>4, regs.v[b2&0xF0>>4])
+			fmt.Printf("Skip next instr if V%X (val:%d) doesn't equal to V%X (val:%d)",
+				b1&0xF, regs.v[b1&0xF], b2&0xF0>>4, regs.v[b2&0xF0>>4])
 			if regs.v[b1&0x0F] != regs.v[b2&0xF0>>4] {
 				regs.progCounter += 2
 				fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -264,19 +268,19 @@ func RunROM() error {
 		case val >= 0xA000 && val <= 0xAFFF:
 			// ANNN	MEM	I = NNN	Sets I to the address NNN.
 			regs.index = val & 0xFFF
-			fmt.Printf("0x%X Set I (memory pointer) to 0x%X (%d)\n", val, val&0xFFF, val&0xFFF)
+			fmt.Printf("Set I (memory pointer) to 0x%X (%d)\n", val&0xFFF, val&0xFFF)
 
 		case val >= 0xB000 && val <= 0xBFFF:
 			// BNNN	Flow	PC=V0+NNN	Jumps to the address NNN plus V0.
 			regs.progCounter = val&0xFFF + uint16(regs.v[0])
-			fmt.Printf("0x%X Jump to address 0x%X plus V0 (val:0x%X) (Final add:0x%X)\n",
-				val, val&0xFFF, regs.v[0], regs.progCounter)
+			fmt.Printf("Jump to address 0x%X plus V0 (val:0x%X) (Final add:0x%X)\n",
+				val&0xFFF, regs.v[0], regs.progCounter)
 
 		case val >= 0xC000 && val <= 0xCFFF:
 			// CXNN	Rand	Vx=rand()&NN	Sets VX to the result of a bitwise and operation
 			// on a random number (Typically: 0 to 255) and NN.
 			regs.v[b1&0xF] = byte(b2 & uint16(rand.Int31n(256)))
-			fmt.Printf("0x%X Set V%X random value 0x%X (%d)\n", val, b1&0xF, regs.v[b1&0xF], regs.v[b1&0xF])
+			fmt.Printf("Set V%X random value 0x%X (%d)\n", b1&0xF, regs.v[b1&0xF], regs.v[b1&0xF])
 
 		case val >= 0xD000 && val <= 0xDFFF:
 			// DXYN	Disp	draw(Vx,Vy,N)	Draws a sprite at coordinate (VX, VY) that
@@ -303,8 +307,8 @@ func RunROM() error {
 				valSlice = append(valSlice, value)
 			}
 
-			fmt.Printf("0x%X Draw a sprite at coor (V%X:%d, V%X:%d) width 8 pixels height %d pixels (valSlice:%d)\n",
-				val, X, regs.v[X], Y, regs.v[Y], height, valSlice)
+			fmt.Printf("Draw a sprite at coor (V%X:%d, V%X:%d) width 8 pixels height %d pixels (valSlice:%d)\n",
+				X, regs.v[X], Y, regs.v[Y], height, valSlice)
 			drawScreen()
 
 		case val >= 0xE000 && val <= 0xEFFF:
@@ -312,7 +316,7 @@ func RunROM() error {
 			case 0x9E:
 				// EX9E	KeyOp	if(key()==Vx)	Skips the next instruction if the key stored in VX is pressed.
 				// (Usually the next instruction is a jump to skip a code block)
-				fmt.Printf("0x%X Skip instruction if key %d is pressed", val, b1&0xF)
+				fmt.Printf("Skip instruction if key %d is pressed", b1&0xF)
 				if keyPressed == regs.v[b1&0x0F] {
 					regs.progCounter += 2
 					fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -322,7 +326,7 @@ func RunROM() error {
 			case 0xA1:
 				//EXA1	KeyOp	if(key()!=Vx)	Skips the next instruction if the key stored in VX
 				// isn't pressed. (Usually the next instruction is a jump to skip a code block)
-				fmt.Printf("0x%X Skip instruction if key %d is not pressed", val, b1&0xF)
+				fmt.Printf("Skip instruction if key %d is not pressed", b1&0xF)
 				if keyPressed != regs.v[b1&0x0F] {
 					regs.progCounter += 2
 					fmt.Printf(" ==> SKIP NEXT INTRUCTION\n")
@@ -337,39 +341,39 @@ func RunROM() error {
 			switch b2 {
 			case 0x0:
 				// Stop
-				fmt.Printf("0x%X Stop\n", val)
+				fmt.Printf("Stop\n")
 				return nil
 			case 0x7:
 				//FX07	Timer	Vx = get_delay()	Sets VX to the value of the delay timer.
 				regs.v[b1&0xF] = getDelay()
-				fmt.Printf("0x%X Set V%X to the value of the delay timer\n", val, b1&0xF)
+				fmt.Printf("Set V%X to the value of the delay timer\n", b1&0xF)
 			case 0xA:
 				//FX0A	KeyOp	Vx = get_key()	A key press is awaited, and then stored in VX.
 				// (Blocking Operation. All instruction halted until next key event)
-				fmt.Printf("0x%X A key press is awaited, and then stored in V%X Blocking operation\n", val, b1&0xF)
+				fmt.Printf("A key press is awaited, and then stored in V%X Blocking operation\n", b1&0xF)
 				keyPressed, _ = readKeyboard()
 				regs.v[b1&0xF] = keyPressed
 				fmt.Printf("Set V%X as %d\n", b1&0xF, regs.v[b1&0xF])
 			case 0x15:
 				// FX15	Timer	delay_timer(Vx)	Sets the delay timer to VX.
 				setDelayTimer(byte(b1) & 0xF)
-				fmt.Printf("0x%X Set the delay timer to V%X (val:%d)\n", val, b1&0xF, regs.v[b1&0xF])
+				fmt.Printf("Set the delay timer to V%X (val:%d)\n", b1&0xF, regs.v[b1&0xF])
 			case 0x18:
 				// FX18	Sound	sound_timer(Vx)	Sets the sound timer to VX.
 				setSoundTimer(byte(b1) & 0xF)
-				fmt.Printf("0x%X Set the sound timer to V%X (val:%d)\n", val, b1&0xF, regs.v[b1&0xF])
+				fmt.Printf("Set the sound timer to V%X (val:%d)\n", b1&0xF, regs.v[b1&0xF])
 			case 0x1E:
 				// FX1E	MEM	I +=Vx	Adds VX to I
 				regs.index = regs.index + uint16(regs.v[b1&0xF])
-				fmt.Printf("0x%X Add V%X (val:%d) to I (final val:%d)\n", val, b1&0xF, regs.v[b1&0xF], regs.index)
+				fmt.Printf("Add V%X (val:%d) to I (final val:%d)\n", b1&0xF, regs.v[b1&0xF], regs.index)
 			case 0x29:
 				// FX29	MEM	I=sprite_addr[Vx]	Sets I to the location of the sprite for the character in VX.
 				// Characters 0-F (in hexadecimal) are represented by a 4x5 font.
 				// All sprites are 5 bytes long, so the location of the specified sprite
 				// is its index multiplied by 5.
 				regs.index = screenMemoryStart + uint16(regs.v[b1&0xF])*5
-				fmt.Printf("0x%X Set I to the location (0x%X) of the sprite for the character in V%X (val:%d)\n",
-					val, regs.index, b1&0xF, regs.v[b1&0xF])
+				fmt.Printf("Set I to the location (0x%X) of the sprite for the character in V%X (val:%d)\n",
+					regs.index, b1&0xF, regs.v[b1&0xF])
 			case 0x33:
 				// FX33	BCD	set_BCD(Vx);
 				// *(I+0)=BCD(3);  *(I+1)=BCD(2);  *(I+2)=BCD(1);
@@ -381,7 +385,7 @@ func RunROM() error {
 				memory[regs.index] = byte(regs.v[b1&0xF] / 100)
 				memory[regs.index+1] = byte(regs.v[b1&0xF]%100) / 10
 				memory[regs.index+2] = regs.v[b1&0xF] % 10
-				fmt.Printf("0x%X Store BCD of V%X (val:%d) at memory index:0x%X)\n", val, b1&0xF, regs.v[b1&0xF], regs.index)
+				fmt.Printf("Store BCD of V%X (val:%d) at memory index:0x%X)\n", b1&0xF, regs.v[b1&0xF], regs.index)
 			case 0x55:
 				// FX55	MEM	reg_dump(Vx,&I)	Stores V0 to VX (including VX) in memory starting at address I.
 				// The offset from I is increased by 1 for each value written, but I itself is left unmodified.
@@ -390,7 +394,7 @@ func RunROM() error {
 					memory[regs.index+i] = regs.v[i]
 					valSlice = append(valSlice, regs.v[i])
 				}
-				fmt.Printf("0x%X Store V0 to V%X (valSlice:%d) in memory starting 0x%X\n", val, b1&0xF, valSlice, regs.index)
+				fmt.Printf("Store V0 to V%X (valSlice:%d) in memory starting 0x%X\n", b1&0xF, valSlice, regs.index)
 			case 0x65:
 				// FX65	MEM	reg_load(Vx,&I)	Fills V0 to VX (including VX) with values from memory starting
 				// at address I. The offset from I is increased by 1 for each value written, but I
@@ -400,7 +404,7 @@ func RunROM() error {
 					regs.v[i] = memory[regs.index+i]
 					valSlice = append(valSlice, regs.v[i])
 				}
-				fmt.Printf("0x%X Fill V0 to V%X with values (valSlice:%d) at memory\n", val, b1&0xF, valSlice)
+				fmt.Printf("Fill V0 to V%X with values (valSlice:%d) at memory\n", b1&0xF, valSlice)
 			default:
 				fmt.Printf("-------------> Unknown statement !!! Data:0x%X\n", val)
 			}
